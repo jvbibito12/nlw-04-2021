74e373a1f8f45dfe0111d4164d4bfe856d64654d
João Victor Ferreira da Silva <jvictorfsilva@gmail.com>
2022-03-01 21:03:17 -0300

    Files

---
+++ public/scripts/intro.js
@@ -0,0 +1,63 @@
+//coments
+// document.write("Hello")
+
+//
+// let myvar = "He"
+// const myconst = "He"
+// document.write(myconst + myvar)
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso é uma string'
+
+//number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1+n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+  // pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+
+// Funções - Funcionalidade - Atallhos - Reuso de código
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Myke")
+// sayMyName("Valeska")
+// sayMyName("Kyan")
+
+//condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+//loop repetições
+//for (i = 0; i < 10; i++) {
+  //document.write('<p>${i}</p>')
+//}
